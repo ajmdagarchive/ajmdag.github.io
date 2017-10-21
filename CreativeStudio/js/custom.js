9bc7dfb762e2f5c8edd7b468f5ea3f498d16c80e
Yury <yurka7321@gmail.com>
2017-10-22 01:17:51 +0500

    Add files via upload

---
+++ CreativeStudio/js/custom.js
@@ -0,0 +1,20 @@
+$(function() {
+	$('.tabs a').click(function(){
+		$(this).parents('.tab-wrap').find('.tab-cont').addClass('hide');
+		$(this).parent().siblings().removeClass('active');
+		var id = $(this).attr('href');
+		$(id).removeClass('hide');
+		$(this).parent().addClass('active');
+		return false;
+	});
+	$('.banner-slider, .testimonial-slider').slick({
+		arrows: false,
+		dots: true,
+	});
+	$('.portfolio-slider').slick({
+		dots: true,
+		appendArrows: '.portfolio-slider__buttons',
+		prevArrow: '<button type="button" class="slick-prev"><i class="fa fa-chevron-left"></i></button>',
+		nextArrow: '<button type="button" class="slick-next"><i class="fa fa-chevron-right"></i></button>'
+	});
+});
